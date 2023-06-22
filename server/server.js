b9c1ec6f412e70ba0938a8b4c734708bd10dd0ab
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-22 10:01:34 +0100

    server - connection established

--- server/server.js
+++ server/server.js
@@ -8,14 +8,17 @@ import {fileURLToPath} from 'url';
 import path from "path";
 dotenv.config()
 
+const app = express()
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const PORT = process.env.PORT
 
-const app = express()
 // Middlewares
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
+// file configuration
+app.use('/assets', express.static(path.join(__dirname , 'public/assets')));
 
 mongoose.connect(process.env.MONGOURL)
 .then(()=>{
