f7c100aeb6a736867e6321879fd0500a4aedea89
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-24 13:56:26 +0100

    Navbar component added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import { useSelector,useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { setLogout } from "../features/user/userSlice";
 
 const Navbar = () => {
-    const user = useSelector((state,action)=> state.user)
+    const {user} = useSelector((state,action)=> state.user)
     const dispatch = useDispatch()
     const navigate = useNavigate()
-
-    console.log(user)
     return (  
           <>
-          <div className="nav__Wrapper">
+          <div className="nav__wrapper">
             <div className="logo">
                 <h2>Scheduley</h2>
             </div>
             <div className="user__details">
-                <img src='' alt="" />
+                <img src={`http://localhost:5000/assets/${user.picturePath}`} alt={user.name} width={50} height={50} style={{borderRadius:"50%",objectFit:"cover"}} />
+                <h4>{user.name}</h4> | <h5>{user.email}</h5>
+                <button onClick={()=>{ dispatch(setLogout()); navigate('/login')}}>Logout</button>
             </div>
           </div>
           </>
