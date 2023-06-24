df2bbda0faec066a96429b20d667f0d2f224167d
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-24 14:24:04 +0100

    Added homePage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import axios from "../services/api";
+import { setTasks } from "../features/task/taskSlice";
 
 const Home = () => {
+    const { tasks } = useSelector(()=> state.task)
+  const dispatch = useDispatch()
+
+ useEffect(()=>{
+     axios.get('/task')
+     .then((res)=>{
+        dispatch(setTasks(res.data.tasks))
+     })
+    },[])
+
     return ( 
            <>
               <Navbar/>
+              <main>
+                
+              </main>
            </>
      );
 }
