5a4cb7108864bf553341dcbd22a3471154a3b33d
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-26 09:50:15 +0100

    added Yup Validation

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -6,6 +6,20 @@ import axios from '../services/api';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const initialEditSchema = Yup.Object().shape({
+    name: Yup.string().required('This is required!'),
+    date: Yup.string().required('This is required!'),
+    type: Yup.string().required('This is required!'),
+    time: Yup.string().required('This is required!'),
+    status: Yup.string().required('This is required!')
+})
+
+const initialCreateSchema = Yup.Object().shape({
+    name: Yup.string().required('This is required!'),
+    date: Yup.string().required('This is required!'),
+    type: Yup.string().required('This is required!'),
+    time: Yup.string().required('This is required!')
+})
 
 const TaskForm = ({ mode='edit',task }) => {
   return (
